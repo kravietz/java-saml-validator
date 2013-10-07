603b3d400b83d848a08f8ed5c3b229317b68bcb3
Pawel Krawczyk <p.krawczyk@kainos.com>
2013-10-07 16:14:04 +0100

    add @Override

--- src/org/owasp/saml/SamlErrorHandler.java
+++ src/org/owasp/saml/SamlErrorHandler.java
@@ -10,16 +10,19 @@ public class SamlErrorHandler implements ErrorHandler {
 
     private final static Logger LOG = Logger.getLogger(SamlErrorHandler.class.getName());
 
+    @Override
     public void warning(SAXParseException e) throws SAXException {
         LOG.warning("Warning " + e.getMessage());
         throw new SAXException("Parser warning " + e.getMessage());
     }
 
+    @Override
     public void error(SAXParseException e) throws SAXException {
         LOG.warning("Error " + e.getMessage());
         throw new SAXException("Parser error " + e.getMessage());
     }
 
+    @Override
     public void fatalError(SAXParseException e) throws SAXException {
         LOG.warning("Fatal " + e.getMessage());
         throw new SAXException("Parser fatal " + e.getMessage());

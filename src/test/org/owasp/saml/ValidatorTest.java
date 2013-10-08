b69347317bf18ae57406c2acf67dd9b5fd0c4a98
Pawel Krawczyk <p.krawczyk@kainos.com>
2013-10-08 17:02:10 +0100

    clean up unnecessary tests

--- src/test/org/owasp/saml/ValidatorTest.java
+++ src/test/org/owasp/saml/ValidatorTest.java
@@ -1,8 +1,19 @@
-package test.org.owasp.saml; 
+/*
+ * Copyright Pawel Krawczyk (c) 2013.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
-import org.junit.Test; 
-import org.junit.Before; 
+package test.org.owasp.saml;
+
 import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.owasp.saml.Validator;
 import org.xml.sax.SAXException;
 
@@ -19,62 +30,40 @@ import java.security.spec.InvalidKeySpecException;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-/** 
-* Validator Tester. 
-* 
-* @author Pawel Krawczyk
-* @since <pre>Oct 7, 2013</pre> 
-* @version 1.0 
-*/ 
-public class ValidatorTest { 
+/**
+ * Validator Tester.
+ *
+ * @author Pawel Krawczyk
+ * @version 1.0
+ * @since <pre>Oct 7, 2013</pre>
+ */
+public class ValidatorTest {
 
-@Before
-public void before() throws Exception { 
-} 
+    @Before
+    public void before() throws Exception {
+    }
 
-@After
-public void after() throws Exception { 
-} 
+    @After
+    public void after() throws Exception {
+    }
 
-/** 
-* 
-* Method: getValidBody() 
-* 
-*/ 
-@Test
-public void testGetValidBody() throws Exception { 
-//TODO: Test goes here... 
-} 
+    /**
+     * Method: validate(final String input)
+     */
+    @Test
+    public void success_on_normal_file() throws SAXException, IOException, ParserConfigurationException, XPathExpressionException, KeyStoreException, NoSuchAlgorithmException, CertificateException, MarshalException, XMLSignatureException, InvalidKeySpecException {
 
-/** 
-* 
-* Method: setIdAttribute(final String ns, final String attr) 
-* 
-*/ 
-@Test
-public void testSetIdAttribute() throws Exception { 
-//TODO: Test goes here... 
-} 
+        Validator val = new Validator("documents/signer1.der", "schemas/soap-envelope.xsd",
+                // basic XPath syntax
+                "/soape:Envelope/soape:Header/wsse:Security/ds:Signature",
+                "/soape:Envelope/soape:Body");
 
-/** 
-* 
-* Method: validate(final String input) 
-* 
-*/
-@Test
-public void success_on_normal_file() throws SAXException, IOException, ParserConfigurationException, XPathExpressionException, KeyStoreException, NoSuchAlgorithmException, CertificateException, MarshalException, XMLSignatureException, InvalidKeySpecException {
+        // soap-envelope.xsd does not specify Id field so XML signature validator will crash if this is not specified
+        val.setIdAttribute("http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd", "Id");
 
-    Validator val = new Validator("documents/signer1.der", "schemas/soap-envelope.xsd",
-            // basic XPath syntax
-            "/soape:Envelope/soape:Header/wsse:Security/ds:Signature",
-            "/soape:Envelope/soape:Body");
+        assertTrue(val.validate("documents/file0.xml"));
 
-    // soap-envelope.xsd does not specify Id field so XML signature validator will crash if this is not specified
-    val.setIdAttribute("http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd", "Id");
-
-    assertTrue(val.validate("documents/file0.xml"));
-
-}
+    }
 
     @Test
     public void fail_on_changed_body() throws SAXException, IOException, ParserConfigurationException, XPathExpressionException, KeyStoreException, NoSuchAlgorithmException, CertificateException, MarshalException, XMLSignatureException, InvalidKeySpecException {
@@ -108,23 +97,4 @@ public void success_on_normal_file() throws SAXException, IOException, ParserCon
     }
 
 
-/** 
-* 
-* Method: toFastXPath(String xpath, Document doc) 
-* 
-*/ 
-@Test
-public void testToFastXPath() throws Exception {
-/* 
-try { 
-   Method method = Validator.getClass().getMethod("toFastXPath", String.class, Document.class); 
-   method.setAccessible(true); 
-   method.invoke(<Object>, <Parameters>); 
-} catch(NoSuchMethodException e) { 
-} catch(IllegalAccessException e) { 
-} catch(InvocationTargetException e) { 
-} 
-*/ 
-} 
-
-} 
+}
